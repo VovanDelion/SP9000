ae20cfb6e762d5a9cb525a1abf4983242c27d9d7
vovan06 <barashewova@gmail.com>
2024-12-24 16:09:05 +0300

    add shooting

--- classes.py
+++ classes.py
@@ -2,15 +2,37 @@ import pygame
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
         self.image = pygame.image.load('sprites/player.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 792
-        self.speed = 5
+        self.screen_width = screen_width
+        self.rect.x = screen_width / 2
+        self.rect.y = screen_height - 30
+        self.clock = pygame.time.Clock()
+        self.delta_time = self.clock.tick(30)
 
-    def move(self, booli):
-        if booli:
-            self.rect.x += self.speed
-        else:
-            self.rect.x -= self.speed
+    def move(self, v):
+        self.rect.x += v
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > self.screen_width - self.rect.width:
+            self.rect.x = self.screen_width - self.rect.width
+
+    def get_xy(self):
+        return self.rect.x + 5, self.rect.y
+
+
+class Bullet():
+    def __init__(self, player):
+        self.image = pygame.image.load('sprites/bullet.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y =  player.get_xy()
+        self.speed = 10
+        self.active = False
+
+
+    def move(self):
+        self.rect.y -= self.speed
+
+    def draw(self, s):
+        s.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import pygame
-
 import classes as c
-
+from classes import Bullet
 
 if __name__ == "__main__":
     pygame.init()
 
-    screen_width = 800
-    screen_height = 800
+    screen_width = 600
+    screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("SP9000")
+    player_v = 5
 
-    player = c.Player()
-
+    player = c.Player(screen_width, screen_height)
+    delta_time = 0
+    bullets = []
     running = True
     clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.rect.x -= player.speed
-                    if player.rect.x <= 0:
-                        player.rect.x = 0
-                elif event.key == pygame.K_RIGHT:
-                    player.rect.x += player.speed
-                    if player.rect.x >= screen_width - player.rect.width:
-                        player.rect.x = screen_width - player.rect.width
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bullets.append(Bullet(player))
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.move(-player_v)
+        elif keys[pygame.K_RIGHT]:
+            player.move(player_v)
+
+        for bullet in bullets[:]:
+            bullet.move()
         screen.fill((0, 0, 0))
         screen.blit(player.image, player.rect)
-        clock.tick(60)
+
+        for bullet in bullets:
+            bullet.draw(screen)
+        clock.tick(30)
         pygame.display.update()
